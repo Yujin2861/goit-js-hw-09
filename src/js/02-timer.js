b962f9adc451d66b29faa609734da1692a7b8567
Romaniuk Yevhen <greenapteka.com.ua@gmail.com>
2023-09-17 16:03:08 +0200

    final

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,56 +1,69 @@
-
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const selectors = {
-input: document.querySelector(`input[type="text"]`),
 
-startBtn: document.querySelector(`button[data-start]`),
 
-days: document.querySelector(`[data-days]`),
-hours: document.querySelector(`[data-hours]`),
-minutes: document.querySelector(`[data-minutes]`),
-seconds: document.querySelector(`[data-seconds]`),
-}
+const startBtn = document.querySelector('button[data-start]');
+console.log(startBtn)
 
-selectors.input.addEventListener("input", onInput)
-function onInput (event) {
-console.log(event.currentTarget.value)
-}
+startBtn.disabled = true;
 
-function flatpickr(selector, options) {
-
-}
+let timerId = null;
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    if (selectedDates[0] <= options.defaultDate) {
+      Notify.failure('Please choose a date in the future');
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+    }
+  },
+};
+const datePicker = flatpickr('#datetime-picker', options);
 
-  function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
-  
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+    startTimer();
+  }, 1000);
+});
+
+function startTimer() {
+  const ms = datePicker.selectedDates[0] - Date.now();
+  const timerTime = convertMs(ms);
+  const { days, hours, minutes, seconds } = timerTime;
+  document.querySelector('span[data-days]').textContent = days;
+  document.querySelector('span[data-hours]').textContent = hours;
+  document.querySelector('span[data-minutes]').textContent = minutes;
+  document.querySelector('span[data-seconds]').textContent = seconds;
+  startBtn.disabled = true;
+
+  if (ms <= 1000) {
+    clearInterval(timerId);
+    }
+};
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+import Notiflix from "notiflix";
+
+const form = document.querySelector('.form')
+
+console.log(form)
+
+form.addEventListener(`submit`, onFormSubmit);
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    let firstDelay = Number(formElements.delay.value);
+    const delayStep = Number(formElements.step.value);
+    const amount = Number(formElements.amount.value);
+
+    for ( let position = 1; position <= amount; position += 1) {
+        createPromise(position, firstDelay);
+        firstDelay += delayStep;
+    };
+   
 }
+
+function createPromise(position, delay) {
+    const promise = new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+  
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+
+    promise.then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      }).catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+  }
